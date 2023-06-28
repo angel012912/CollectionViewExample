db516377f039b8cedcd8c5833a2565164d09e4a7
angel012912 <angel_012912@hotmail.com>
2023-06-27 22:27:16 -0600

    Initial Commit

---
+++ CollectionViewExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewExample
+//
+//  Created by Angel Garcia on 27/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
